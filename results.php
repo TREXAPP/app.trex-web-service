cbbd0e8856865f44fe4c8f804cee266a755b8a02
igority <igority@gmail.com>
2017-08-28 23:57:15 +0200

    results.php
    results is done, it functions. Not tested. No css.

--- results.php
+++ results.php
@@ -13,19 +13,16 @@ display - [live,final] - dali da prikaze live rezultati i officijalni
 ?>
 <html>
 <head>
+<title>Results - Krali Marko Trails 2017</title>
+<meta name="Description" content="Live and Official results from the Krali Marko Trails 2017 race, Macedonia">
+<meta name="Keywords" content="Krali Marko, Krali Marko Trails, KMT, KMT2017, Running, Trail Running, Ultra Marathon, Macedonia, Ultra Trail Running">
+<link rel="stylesheet" href="/style_results.css">
 </head>
 <body>
 
+
 <?php
-/************
-testing sort
-***********/
 
-$testArray = 
-
-/************
-testing end
-***********/
 require 'conn.php';
 
 if (isset($_GET['RaceGroupID'])) {
@@ -36,7 +33,7 @@ if (isset($_GET['RaceGroupID'])) {
 	*/
 
 	
-	
+	$StartTime = NULL;
 	$RaceGroupID = $_GET['RaceGroupID'];
 	$raceName = GetRaceName($RaceGroupID);
 	echo DrawHeader($raceName);
@@ -44,13 +41,14 @@ if (isset($_GET['RaceGroupID'])) {
 	
 	$query = "SELECT * FROM Races WHERE RaceGroupID = " . $RaceGroupID;
 	$races_result = mysqli_query($conn,$query);
-	 
-	echo DrawSubRacesList($races_result, $RaceGroupID);
+	$races = getRaces($races_result);
+	echo DrawSubRacesList($races, $RaceGroupID);
 	
-		
 	$drawTable = false;
 	if ($races_result->num_rows  == 1) {
-		$row = $racegroups_result->fetch_array(MYSQL_ASSOC);
+		$row = $races_result->fetch_array(MYSQL_ASSOC);
+		//mysql_data_seek($races_result, 0);
+		//mysql_data_seek($racegroups_result, 0);
 		$RaceID = $row['RaceID'];
 		$drawTable = true;
 	}
@@ -65,16 +63,17 @@ if (isset($_GET['RaceGroupID'])) {
 			$display = $_GET['display'];
 		}
 		
+
 		//draw table here
 		$RaceID = $_GET['RaceID'];
+		$StartTime = getStartTime($races, $RaceID, $RaceGroupID);
 		$headerIDsArray = array();
 		$headerArray = getHeaderArray($RaceGroupID, $RaceID, $display, $headerIDsArray);
-		$tableArray = getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display);
-		sortTableArray($tableArray);
-		echo DrawTheTable($headerArray, $tableArray, $display);
+		$tableArray = getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display, $StartTime);
+		$sortedTableArray = sortTableArray($tableArray, $headerArray, $StartTime);
+		echo DrawTheTable($headerArray, $sortedTableArray, $display);
 	}
-	/*
-	*/
+	
 } else {
 	
 	$query = "SELECT * FROM `RaceGroups` WHERE 1";
@@ -102,6 +101,24 @@ if (isset($_GET['RaceGroupID'])) {
 </html>
 
 <?php
+function getRaces($races_result) {
+	$races = array();
+	$racesCount = 0;
+	if ($races_result->num_rows > 0) {
+		while($row = $races_result->fetch_array(MYSQL_ASSOC)) {
+			$races[$racesCount]['RaceID'] = $row['RaceID'];
+			$races[$racesCount]['RaceGroupID'] = $row['RaceGroupID'];
+			$races[$racesCount]['RaceName'] = $row['RaceName'];
+			$races[$racesCount]['Description'] = $row['Description'];
+			$races[$racesCount]['StartTime'] = $row['StartTime'];
+			$racesCount++;
+		}
+		return $races;
+	} else {
+		return null;
+	}
+}
+
 function GetRaceName($RaceGroupID) {
 	require 'conn.php';
 	$racegroups_query = "SELECT * FROM `RaceGroups` WHERE `RaceGroups`.RaceGroupID='" . $RaceGroupID . "';";
@@ -125,7 +142,7 @@ function DrawHeader($raceName) {
 	return $returnStr;
 }
 
-function getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display) {
+function getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display, $StartTime) {
 	
 	require 'conn.php';
 
@@ -150,24 +167,29 @@ function getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display) {
 		while($row = $racers_result->fetch_array(MYSQL_ASSOC)) {
 			$racersArray[] = $row;
 			/*
-				$resultArray[0] = "Position";
-				$resultArray[1] = "Starting number";
-				$resultArray[2] = "Name";
-				$resultArray[3] = "Gender";
-				$resultArray[4] = "Country";
+				$resultArray[0] = "Position (Total)";
+				$resultArray[1] = "Position (Gender)";
+				$resultArray[2] = "Starting number";
+				$resultArray[3] = "Name";
+				$resultArray[4] = "Gender";
+				$resultArray[5] = "Country";
 			*/
 			
 			$returnArray[$racersCounter][0] = $racersCounter+1;
-			$returnArray[$racersCounter][1] = $row['BIB'];
-			$returnArray[$racersCounter][2] = $row['Name'];
-			$returnArray[$racersCounter][3] = $row['Gender'];
-			$returnArray[$racersCounter][4] = $row['Country'];
+			$returnArray[$racersCounter][1] = $racersCounter+1;
+			$returnArray[$racersCounter][2] = $row['BIB'];
+			$returnArray[$racersCounter][3] = $row['Name'];
+			$returnArray[$racersCounter][4] = $row['Gender'];
+			$returnArray[$racersCounter][5] = $row['Country'];
 			
 			$CPCounter = 0;
 			while (isset($headerIDsArray[$CPCounter])) {			
-				$returnArray[$racersCounter][$CPCounter+5] = findTheEntryTime($row['ActiveRacerID'],$headerIDsArray[$CPCounter],$entriesArray);
+				$returnArray[$racersCounter][$CPCounter+6] = findTheEntryTime($row['ActiveRacerID'],$headerIDsArray[$CPCounter],$entriesArray);
 				$CPCounter++;
 			}
+			
+			//avg speed
+			$returnArray[$racersCounter][$CPCounter+6] = "--";
 			$racersCounter++;
 		}	
 
@@ -179,8 +201,7 @@ function getTableArray($headerIDsArray, $RaceGroupID, $RaceID, $display) {
 	return $returnArray;
 }
 
-function sortTableArray(&$tableArray) {
-	var_dump($tableArray);
+function sortTableArray(&$tableArray, $headerArray, $StartTime) {
 	
 	
 	
@@ -199,84 +220,104 @@ function sortTableArray(&$tableArray) {
 	$stTotalCount = 0;
 	//2 ja vrtime tableArray na sledniov nacin:
 	//3 sekoja kolona, pocnuvajki od poslednata (finish) pa se do 1 kontrolna
-	for ($i=count($tableArray[0])-1;$i>4;$i--) {
-		echo "<br>za kolona so red. broj: " . $i . "<br>";		
+	for ($i=count($tableArray[0])-1;$i>5;$i--) {
 		//4 zemame predvid samo ako postoi vreme (ne e prazno)
 		//5 stavi gi vo privremena niza ($sortColumn) samo tie sto imaat vreminja
 		$sortColumn = array ();
 		$stColumnCount = 0;
-		for ($j=0;$j<count($tableArray);$j++) {
-			echo "<br>red: " . $j . "<br>";
-			echo "<br>vreme: " . $tableArray[$j][$i] . "<br>";
+		for ($j=0;$j<count($tableArray)-1;$j++) {
 			if (($tableArray[$j][$i] != "--") && ($tableArray[$j][$i] != "") && (!is_null($tableArray[$j][$i]))) {
-				echo "<br>go dodavame vo nizata \$sortColumn<br>";
-				$sortColumn['BIB'][$stColumnCount] = $tableArray[$j][1];
-				$sortColumn['Time'][$stColumnCount] = $tableArray[$j][$i];
+				$sortColumn[$stColumnCount]['BIB'] = $tableArray[$j][2];
+				$sortColumn[$stColumnCount]['Time'] = $tableArray[$j][$i];
+				$sortColumn[$stColumnCount]['Timestamp'] = strtotime($tableArray[$j][$i]);
+				$distance = $headerArray[$i]['Distance'];
+				if ($distance != null) {
+					$sortColumn[$stColumnCount]['AvgSpeed'] = getAvgSpeed($sortColumn[$stColumnCount]['Timestamp'], $StartTime, $distance);
+				} else {
+					$sortColumn[$stColumnCount]['AvgSpeed'] = '--';
+				}
 				$stColumnCount++;
 			} else {
-				echo "<br>ne go dodavame vo \$sortColumn<br>";
 			}
 		}
-		echo "pred:<br>";
-		var_dump($sortColumn);
-				
-		usort($sortColumn['Time'], function($a, $b) {
-			return strcmp($a['Time'], $b['Time']);
-		});
-		//TODO
-		// NAPRAVI JA NIZATA DA BIDE [0]['BIB'], [0]{Time] namesto [BIB][0], [Time][0], i posle sortiraj so usort!!!!!!!!!!!!!!!!!!!!!!!!
-		usort($sortColumn['BIB'], function($a, $b) {
-			return strcmp($a['Time'], $b['Time']);
-		});
-/*
+
 		//6 sortiraj ja $sortColumn od najmal do najgolem spored 'Time'
-		// Obtain a list of columns
-		foreach ($sortColumn as $key => $row) {
-			$Time[$key]  = $row['Time'];
-			$Timestamp[$key]  = strtotime($row['Time']);
-			$BIB[$key]  = $row['BIB'];
-			
-			//strtotime
+		usort($sortColumn, function($a, $b) {
+			return strcmp($a['Timestamp'], $b['Timestamp']);
+		});
+
+		
+		//7 proveri go sekoj clen od 0 nagore dali postoi vo $sortTotal. Ako ne, dodadi go. Ako postoi, ignore
+		if ($stColumnCount > 0) {
+			foreach ($sortColumn as &$row) {
+				$sortTotal[$stColumnCount]['BIB'] = $row['BIB'];
+				$sortTotal[$stColumnCount]['Time'] = $row['Time'];
+				$sortTotal[$stColumnCount]['AvgSpeed'] = $row['AvgSpeed'];
+				$stColumnCount++;
+			}
 		}
-*/
-		// Sort the data with volume descending, edition ascending
-		// Add $data as the last parameter, to sort by the common key
-		//array_multisort($Timestamp, SORT_ASC, $BIB, SORT_ASC, $sortColumn['BIB'], $sortColumn['Time']);
-		//array_multisort($Timestamp, SORT_ASC,$sortColumn['Time'], $sortColumn['BIB'], );
-		//array_multisort($sortColumn['Time'], SORT_ASC, $sortColumn);
-		echo "<br>posle:<br>";
-		var_dump($sortColumn);
 	}
-
 	
+	$sortedTableArray = array ();
+	$sortedIndex = 0;
+	$positionMale = 1;
+	$positionFemale = 1;
+	
+	//8 Otkako $sortTotal e formirana, nov for ciklus koj sto ke ja sortira $tableArray spored redosledot vo $sortTotal.
+	foreach ($sortTotal as &$sortTotalRow) {
+		$found = false;
+		foreach ($tableArray as &$tableArrayRow) {
+			
+			if (!$found && $sortTotalRow['BIB'] == $tableArrayRow[2]) {
+				$sortedTableArray[$sortedIndex] = $tableArrayRow;
+				//Position (total)
+				$sortedTableArray[$sortedIndex][0] = $sortedIndex+1;
+				//Position (gender)
+				if ($sortedTableArray[$sortedIndex][4] == 'F') {
+					$sortedTableArray[$sortedIndex][1] = $positionFemale;
+					$positionFemale++;
+				} else {
+					$sortedTableArray[$sortedIndex][1] = $positionMale;
+					$positionMale++;
+				}
+				//Avg Speed
+				$sortedTableArray[$sortedIndex][count($tableArrayRow)-1] = $sortTotalRow['AvgSpeed'];
+				$sortedIndex++;
+				$found = true;
+			}
+		}
+	}
+	
+	//9 i na kraj uste eden for ciklus sto ke proveri ako nekoj go nema vo $sortTotal, 
+	//a go ima vo $tableArray (ne pominal niz ni edna kontrolna do sega) da go dodade na kraj, po redosled na BIB
+	
+	foreach ($tableArray as &$tableArrayRow) {
+	$found = false;
+		foreach ($sortTotal as &$sortTotalRow) {
+			if (!$found && $sortTotalRow['BIB'] == $tableArrayRow[2]) {
+				$found = true;
+			}
+		}
+		
+		if (!$found) {
+				$sortedTableArray[$sortedIndex] = $tableArrayRow;
+				
+				$sortedTableArray[$sortedIndex][0] = $sortedIndex+1;
+				if ($sortedTableArray[$sortedIndex][4] == 'F') {
+					$sortedTableArray[$sortedIndex][1] = $positionFemale;
+					$positionFemale++;
+				} else {
+					$sortedTableArray[$sortedIndex][1] = $positionMale;
+					$positionMale++;
+				}
+				
+				$sortedIndex++;
+		}
+	}
+	
+	
+	return $sortedTableArray;
 }
-
-function array_msort($array, $cols)
-{
-    $colarr = array();
-    foreach ($cols as $col => $order) {
-        $colarr[$col] = array();
-        foreach ($array as $k => $row) { $colarr[$col]['_'.$k] = strtolower($row[$col]); }
-    }
-    $eval = 'array_multisort(';
-    foreach ($cols as $col => $order) {
-        $eval .= '$colarr[\''.$col.'\'],'.$order.',';
-    }
-    $eval = substr($eval,0,-1).');';
-    eval($eval);
-    $ret = array();
-    foreach ($colarr as $col => $arr) {
-        foreach ($arr as $k => $v) {
-            $k = substr($k,1);
-            if (!isset($ret[$k])) $ret[$k] = $array[$k];
-            $ret[$k][$col] = $array[$k][$col];
-        }
-    }
-    return $ret;
-
-}
-
-
 
 function findTheEntryTime($ActiveRacerID, $CPNo, $entriesArray) {
 	$i=0;
@@ -302,7 +343,7 @@ function getHeaderArray($RaceGroupID, $RaceID, $display, &$headerIDsArray) {
 	require 'conn.php';
 	
 	//retrieve header fields
-	$q = "SELECT CONCAT(CPNo, ' ', CPName) AS CPNoDisplay, CPNo FROM `RacesControlPoints` 
+	$q = "SELECT CONCAT(CPNo, ' ', CPName) AS CPNoDisplay, CPNo, Distance FROM `RacesControlPoints` 
 		JOIN ControlPoints ON RacesControlPoints.CPID = ControlPoints.CPID
 		WHERE RaceID = " . $RaceID;
 	$header_result = mysqli_query($conn,$q);
@@ -319,6 +360,7 @@ function getHeaderArray($RaceGroupID, $RaceID, $display, &$headerIDsArray) {
 			$replace   = array("00", "99");
 			$cpnoDisplayArray[$i] = str_replace($find, $replace, $row["CPNoDisplay"]);
 			$cpnoArray[$i] = str_replace($find, $replace, $row["CPNo"]);
+			$distanceArray[$i] = $row['Distance'];
 			$i++;
 		}
 
@@ -326,24 +368,33 @@ function getHeaderArray($RaceGroupID, $RaceID, $display, &$headerIDsArray) {
 	} else {
 		//return null;
 	}
+
+	sort($distanceArray);
 	sort($cpnoDisplayArray);
 	sort($cpnoArray);
-		$i=0;
+
+	$i=0;
 		while (isset($cpnoArray[$i])) {
 			$headerIDsArray[$i] = str_replace($replace, $find, $cpnoArray[$i]);
 			$i++;
 		}
 	//add other fields in front
-	$resultArray[0] = "Position";
-	$resultArray[1] = "Starting number";
-	$resultArray[2] = "Name";
-	$resultArray[3] = "Gender";
-	$resultArray[4] = "Country";
+	$resultArray[0]['Value'] = "Position (Total)";
+	$resultArray[1]['Value'] = "Position (Gender)";
+	$resultArray[2]['Value'] = "Starting number";
+	$resultArray[3]['Value'] = "Name";
+	$resultArray[4]['Value'] = "Gender";
+	$resultArray[5]['Value'] = "Country";
 	$i=0;
 		while (isset($cpnoDisplayArray[$i])) {
-			$resultArray[$i+5] = str_replace($replace, $find, $cpnoDisplayArray[$i]);
+			$resultArray[$i+6]['Value'] = str_replace($replace, $find, $cpnoDisplayArray[$i]);
+			$resultArray[$i+6]['Distance'] = $distanceArray[$i];
 			$i++;
 		}
+		
+		//Avg speed
+		$resultArray[$i+6]['Value'] = "Average Speed";
+		$i++;
 	
 	return $resultArray;
 }
@@ -356,9 +407,9 @@ function DrawTheTable($headerArray, $tableArray, $display) {
 			<tbody>
 				<tr>";
 		$headerCounter = 0;
-		while (isset($headerArray[$headerCounter])) {
+		while (isset($headerArray[$headerCounter]['Value'])) {
 			$returnStr .= "	<th>";
-			$returnStr .= $headerArray[$headerCounter];
+			$returnStr .= $headerArray[$headerCounter]['Value'];
 			$returnStr .= "	</th>";
 			
 			$headerCounter++;
@@ -386,11 +437,12 @@ function DrawTheTable($headerArray, $tableArray, $display) {
 	return $returnStr;
 }
 
-function DrawSubRacesList($races_result, $RaceGroupID) {
+function DrawSubRacesList($races, $RaceGroupID) {
 	$returnStr = "";
 	$returnStr .= "<div class='subraces_list'>";
 	
 	$returnStr .=  "<ul>";
+	/*
 		while($row = $races_result->fetch_array(MYSQL_ASSOC)) {
 			$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 			$subrace_link = $actual_link . "?RaceGroupID=" . $RaceGroupID . "&RaceID=" . $row['RaceID'];
@@ -402,12 +454,37 @@ function DrawSubRacesList($races_result, $RaceGroupID) {
 			$returnStr .=  "<a href='" . $subrace_link .  "&display=final'>OFFICIAL RESULTS</a>";
 			$returnStr .=  "</li>";
 		}	
+	*/
+	
+	foreach ($races as &$row) {
+		$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+		$subrace_link = $actual_link . "?RaceGroupID=" . $RaceGroupID . "&RaceID=" . $row['RaceID'];
+		$returnStr .=  "<li>";
+		$returnStr .= $row['Description'];
+		$returnStr .= "<br/>";
+		$returnStr .=  "<a href='" . $subrace_link .  "&display=live'>LIVE RESULTS</a>";
+		$returnStr .= " | ";
+		$returnStr .=  "<a href='" . $subrace_link .  "&display=final'>OFFICIAL RESULTS</a>";
+		$returnStr .=  "</li>";
+	}
 		$returnStr .=  "</ul>";
 	
 	
 	$returnStr .= "</div>";
-	
+
 	return $returnStr;
+}
+
+function getStartTime($races, $RaceID, $RaceGroupID) {
+	//mysql_data_seek($races, 0);
+	
+	foreach ($races as $row) {
+		if ($row['RaceID'] == $RaceID && $row['RaceGroupID'] == $RaceGroupID) {
+			return $row['StartTime'];
+		}
+	}
+	return null;
+	
 }
 
 function formatTime($rawValue) {
@@ -416,4 +493,18 @@ function formatTime($rawValue) {
 	return $rawValue;
 }
 
+function getAvgSpeed($lastTimestamp, $StartTime, $distance) {
+	
+	$seconds = (int)$lastTimestamp - (int)strtotime($StartTime);
+	$kmh = $distance*3600/$seconds;
+	
+	$minkm_num = $seconds/($distance*60);
+	$min = floor($minkm_num);
+	$sec = floor(($minkm_num - floor($minkm_num))*60);
+	if ($sec < 10) {
+		$sec = '0' . $sec;
+	}
+	$minkm_str = $min . ":" . $sec;
+	return number_format($kmh,2) . " km/h | " . $minkm_str . " min/km";
+}
 ?>
