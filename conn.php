62e277bf900f41eabd643ba364f819084682a0e8
igority <igority@gmail.com>
2017-09-06 11:02:40 +0200

    conn

--- conn.php
+++ conn.php
@@ -8,12 +8,12 @@ $database = "sidmk_trexapp";
  
 // Create connection
 $conn=mysqli_connect($servername,$username,$password,$database);
-
-if (mysqli_connect_errno($con))
+mysqli_set_charset($conn,"utf8");
+if (mysqli_connect_errno($conn))
 {
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 } else { 
 //echo "db success<br/><br/>";
 }
-
+mysql_query("SET NAMES UTF8");
 ?>
